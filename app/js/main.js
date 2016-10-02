a89e5f7629cbb23fc68bdee8c33d306693ccf8f7
severjason <ssever2010@gmail.com>
2016-10-02 21:17:19 +0300

    rewrited js from jquery to native js

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,22 @@
-(function ($) {
-    $(document).ready(function () {
+"use strict";
+(function () {
+    document.addEventListener('DOMContentLoaded', function () {
 
-        let mainObject = new function () {
-            this.labArray = [];
-            this.mainContent = $("#main_content");
-            this.nav = $("nav");
-            this.navLink = document.querySelectorAll("ul.nav-tabs > li");
-            this.quantityPerSide = 20;
-            this.random = function (min, max) {
+        let labyrinth = new function () {
+            let labArray = [];
+            let mainContent = document.getElementById("main_content");
+            //this.navLink = document.querySelectorAll("ul.nav-tabs > li");
+            let quantityPerSide = 20;
+            this.random = function (min, max, exclude = false) {
                 let rand = min - 0.5 + Math.random() * (max - min + 1);
                 rand = Math.round(Math.abs(rand));
+                while (exclude === rand) {
+                    rand = min - 0.5 + Math.random() * (max - min + 1);
+                    rand = Math.round(Math.abs(rand));
+                }
                 return rand;
             };
-            this.getCLass = function (number) {
+            this.getClass = function (number) {
                 switch (number) {
                     case 0:
                         return "top ";
@@ -28,170 +32,173 @@
                         break;
                     default:
                         return "empty ";
-                    break;
+                        break;
                 }
             };
-            this.generateLabyrinth = function () {
-                let lab = document.getElementById("labyrinth");
-                let labWidth = lab.offsetWidth;
-                lab.innerHTML = " ";
-                this.quantityPerSide = $("#labyrinth_level").find("option:selected").data("level");
+            this.generate = function () {
+                let labID = document.getElementById("labyrinth");
+                let labWidth = labID.offsetWidth;
+                labID.innerHTML = "";
+                quantityPerSide = document.querySelector("#labyrinth_level option:checked").dataset.level;
 
-                createArray();
-                for (let i = 0; i < this.quantityPerSide; i++) {
-                    for (let j = 0; j < this.quantityPerSide; j++) {
-                        lab.innerHTML+= "<div class='square'></div>";
-                        let innerDiv = document.querySelector("div.square");
-                        innerDiv.style.width = Math.floor(labWidth / this.quantityPerSide) + "px";
-                        //innerDiv.style["height"] = Math.floor(labWidth / this.quantityPerSide);
+                createLabArray();
 
+                for (let i = 0; i < quantityPerSide; i++) {
 
-                        /*lab.append(
-                            $(innerDiv)
-                                .css({
-                                    width: Math.floor(labWidth / this.quantityPerSide),
-                                    height: Math.floor(labWidth / this.quantityPerSide)
-                                })
-                                .addClass(this.labArray[i][j])
-                        );*/
+                    for (let j = 0; j < quantityPerSide; j++) {
+
+                        let newDiv = document.createElement("div");
+
+                        newDiv.className = "square ";
+                        newDiv.className += labArray[i][j];
+                        let CSS = {
+                            "width": Math.floor(labWidth / quantityPerSide) + "px",
+                            "height": Math.floor(labWidth / quantityPerSide) + "px"
+                        };
+
+                        for (let attribute in CSS) {
+
+                            if (CSS.hasOwnProperty(attribute)) {
+                                newDiv.style[attribute] = CSS[attribute];
+                            }
+                        }
+
+                        labID.appendChild(newDiv);
                     }
                 }
-/*                for (let i = 0; i < this.quantityPerSide * this.quantityPerSide; i++) {
-                    lab.append(
-                        $(innerDiv)
-                            .css({
-                                width: Math.floor(labWidth / this.quantityPerSide),
-                                height: Math.floor(labWidth / this.quantityPerSide)
-                            })
-                            .addClass("br_" + this.random(1, 4) % 4)
-                    );
-                }*/
             };
             this.ajaxLoad = function (page) {
+
                 if (page != undefined) {
-                    $.ajax({
-                        url: "partials/" + page + ".html",
-                        type: "GET",
-                        dataType: "html",
-                        beforeSend: function () {
-                            mainObject.mainContent.empty();
-                        },
-                        error: function () {
-                            mainObject.mainContent.html('<span class="">DATA LOADING ERROR!</span>');
-                        },
-                        success: function (response) {
-                            mainObject.mainContent.html(response);
+
+                    mainContent.innerHTML = "";
+
+                    var request = new XMLHttpRequest();
+
+                    request.open("GET", "partials/" + page + ".html", true);
+
+                    request.onprogress = function () {
+                        mainContent.innerHTML = "<span class='ajaxInfo'>Loading...</span>";
+                    };
+
+                    request.onload = function (e) {
+
+                        if (request.readyState === 4) {
+
+                            // Check if the get was successful.
+                            if (request.status === 200) {
+                                mainContent.innerHTML = request.responseText;
+                            } else {
+                                mainContent.innerHTML = "<span class='ajaxInfo'>DATA LOADING ERROR!</span>";
+                                console.error(request.statusText);
+                            }
                         }
-                    });
+                    };
+
+                    request.send(null);
                 }
-            }
-        };
 
+            };
+            let createLabArray = function () {
+                labArray = [];
+                for (let i = 0; i < quantityPerSide; i++) {
 
-        let createArray = function () {
-            mainObject.labArray = [];
-            for (let i = 0; i < mainObject.quantityPerSide; i++) {
+                    let row = [];
 
-                let row = [];
+                    /*
+                     top - 0
+                     bottom - 1
+                     left - 2
+                     right - 3
+                     */
+                    let topLineSwitcher = 0;
+                    for (let j = 0; j < quantityPerSide; j++) {
+                        let newElement = labyrinth.getClass(labyrinth.random(0, 3));
 
-                /*
-                 top - 0
-                 bottom - 1
-                 left - 2
-                 right - 3
-                 */
-                let topLineSwitcher = 0;
-                for (let j = 0; j < mainObject.quantityPerSide; j++) {
+                        if (i === 0) {
 
-                    if (i === 0) {
-                        let newElement = mainObject.getCLass(mainObject.random(0, 3));
-
-                        if (j === 0 ) {
-                            row.push(mainObject.getCLass(0) + mainObject.getCLass(2));
+                            if (j === 0) {
+                                row.push(labyrinth.getClass(0) + labyrinth.getClass(2));
+                            }
+                            else if (j === quantityPerSide - 1) {
+                                row.push(labyrinth.getClass(0) + labyrinth.getClass(3));
+                            }
+                            else {
+                                /* if (newElement !== labyrinth.getClass(0)) {
+                                 topLineSwitcher++;
+                                 }
+                                 console.log(topLineSwitcher);
+                                 if (topLineSwitcher <= 2) {
+                                 row.push(labyrinth.getClass(labyrinth.random(1, 3)));
+                                 }
+                                 else {
+                                 row.push(labyrinth.getClass(0));
+                                 }*/
+                                row.push(labyrinth.getClass(labyrinth.random(1, 3)));
+                            }
                         }
-                        else if (j === mainObject.quantityPerSide - 1) {
-                            row.push(mainObject.getCLass(0) + mainObject.getCLass(3));
+                        else if (j === 0) {
+                            if (i === quantityPerSide - 1) {
+                                row.push(labyrinth.getClass(1) + labyrinth.getClass(2));
+                            }
+                            else {
+                                row.push(labyrinth.getClass(2) + labyrinth.getClass(labyrinth.random(0, 3, 2)));
+                            }
+                        }
+                        else if (j === quantityPerSide - 1) {
+                            if (i === quantityPerSide - 1) {
+                                row.push(labyrinth.getClass(1) + labyrinth.getClass(3));
+                            }
+                            else {
+                                row.push(labyrinth.getClass(3) + labyrinth.getClass(labyrinth.random(0, 2)));
+                            }
                         }
                         else {
-                            /* if (newElement !== mainObject.getCLass(0)) {
-                             topLineSwitcher++;
-                             }
-                             console.log(topLineSwitcher);
-                             if (topLineSwitcher <= 2) {
-                             row.push(mainObject.getCLass(mainObject.random(1, 3)));
-                             }
-                             else {
-                             row.push(mainObject.getCLass(0));
-                             }*/
-                            row.push(mainObject.getCLass(mainObject.random(1, 3)));
+                            row.push(newElement);
                         }
-                    }
-                    else if (i === mainObject.quantityPerSide - 1 && j === 0) {
-                        row.push(mainObject.getCLass(1) + mainObject.getCLass(2));
-                    }
-                    else if (i === mainObject.quantityPerSide - 1 && j === mainObject.quantityPerSide - 1) {
-                        row.push(mainObject.getCLass(1) + mainObject.getCLass(3));
-                    }
-                    else {
-                        row.push(mainObject.getCLass(mainObject.random(0, 3)));
-                    }
 
+                    }
+                    labArray.push(row);
+                    row = [];
                 }
-                mainObject.labArray.push(row);
-                row = [];
-            }
+            };
         };
 
 
-
-
-        //console.table(mainObject.labArray);
-
-        $(document).on("change", mainObject.level, function () {
-            mainObject.generateLabyrinth();
+        document.querySelector("#labyrinth_level").addEventListener("change", function () {
+            labyrinth.generate();
         });
 
-        $(document).on("click", "button#generate", function () {
-            mainObject.generateLabyrinth();
+        document.querySelector("button#generate").addEventListener("click", function () {
+            labyrinth.generate();
         });
 
-        $(document).on("click", "ul.nav-tabs > li", function () {
-            mainObject.nav.find("li").removeClass("active");
-            $(this).addClass("active");
-            let page = $(this).data("page");
-            let defDiv = $("#default");
-            if (page != undefined) {
-                defDiv.hide();
-                mainObject.ajaxLoad(page);
-            }
-            else {
-                mainObject.mainContent.empty();
-                defDiv.show();
-            }
+        document.querySelectorAll("ul.nav-tabs li").forEach(function (li) {
+
+            li.addEventListener("click", function () {
+                document.querySelectorAll("nav li").forEach(function (li) {
+                    li.classList.remove("active")
+                });
+
+                this.classList.add("active");
+
+                let page = this.dataset.page;
+
+                let defDiv = document.getElementById("default");
+
+                if (page != undefined) {
+                    defDiv.style.display = "none";
+                    labyrinth.ajaxLoad(page);
+                }
+                else {
+                    document.getElementById("main_content").innerHTML = "";
+                    defDiv.style.display = "block";
+                }
+
+            })
         });
-
-
-/*
-        mainObject.navLink.on("click", function () {
-            mainObject.nav.find("li").removeClass("active");
-            $(this).addClass("active");
-            let page = $(this).data("page");
-            let defDiv = $("#default");
-            if (page != undefined) {
-                defDiv.hide();
-                mainObject.ajaxLoad(page);
-            }
-            else {
-                mainObject.mainContent.empty();
-                defDiv.show();
-            }
-        });
-*/
-
-
-
-
+        
     });
 
 
-})(jQuery);
+})();
